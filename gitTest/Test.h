d023d3d6cdf169d45fcf7b0f160bdb1594680f2b
Manav Pavitra Singh <manavpavitrasingh@Manavs-MacBook-Pro.local>
2015-03-04 17:54:40 -0800

    added a test class

---
+++ gitTest/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  gitTest
+//
+//  Created by Manav Pavitra Singh on 3/4/15.
+//  Copyright (c) 2015 SJSU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ gitTest/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  gitTest
+//
+//  Created by Manav Pavitra Singh on 3/4/15.
+//  Copyright (c) 2015 SJSU. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
